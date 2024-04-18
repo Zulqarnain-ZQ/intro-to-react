8fd13d101bf80959c054ae3305ae433df57cbb71
Muhammad Zulqarnain <zulqarnain.fastian@gmail.com>
2024-04-18 14:21:18 +0500

    Refactor MyIntro component

--- lecture2-react/src/App.js
+++ lecture2-react/src/App.js
@@ -3,30 +3,22 @@ import "./App.css";
 import { First } from "./First";
 import { Second } from "./Second";
 import TestClass from "./TestClass";
-import { PropComponent } from "./PropComponent";
+import { MyIntro } from "./MyIntro";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
+    <div
+      style={{
+        padding: "20px",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <First />
       <Second />
       <TestClass />
-      <PropComponent
+      <MyIntro
         firstName="Muhammad"
         lastName="Zulqarnain"
         age={30}
--- lecture2-react/src/Second.jsx
+++ lecture2-react/src/Second.jsx
@@ -1,4 +1,3 @@
-import { First } from "./First"
 import styles from "./Second.module.css"
 
 export function Second(){
@@ -7,6 +6,5 @@ export function Second(){
 
     return <div className={styles.container}>
         Hello from functional component
-        <First />
         </div>
 }
