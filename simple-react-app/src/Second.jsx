88b9e4dba5075606b64eb8f9e033c4f8b08ecf5e
Muhammad Zulqarnain <zulqarnain.fastian@gmail.com>
2024-04-18 11:52:33 +0500

    Lecture 2 changes

---
+++ simple-react-app/src/Second.jsx
@@ -0,0 +1,8 @@
+import { First } from "./First"
+
+export function Second(){
+    return <div>
+        Hello from functional component
+        <First />
+        </div>
+}
---
+++ simple-react-app/src/TestClass.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class TestClass extends Component {
+    render(){
+        return <div> Hello this is class based component</div>
+    }
+}
+
+export default TestClass
