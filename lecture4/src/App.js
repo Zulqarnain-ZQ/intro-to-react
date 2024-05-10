84476738ae1456d7255933f7f6d007318aff99b7
Muhammad Zulqarnain <zulqarnain.fastian@gmail.com>
2024-05-10 13:26:28 +0500

    Add react router dom and routes

--- lecture4/src/App.js
+++ lecture4/src/App.js
@@ -10,7 +10,8 @@ import { FormInput } from "./FormInput";
 import { Calculator } from "./Calculator";
 import { Carousel } from "./Carousel";
 import { TipOfTheDay } from "./TipOfTheDay";
-import {PokemonList} from "./PokemonList";
+import { PokemonList } from "./PokemonList";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const products = [
   {
@@ -37,49 +38,73 @@ function App() {
   const [userData, setUserData] = useState(null);
   const [isSaving, setIsSaving] = useState(false);
 
-  // console.log("userData", userData);
+  // // console.log("userData", userData);
 
   function onSaveData(user) {
     setUserData(user);
   }
 
+  // return (
+  //   <div
+  //     style={{
+  //       padding: "20px",
+  //       backgroundColor: "white",
+  //       display: "flex",
+  //       flexDirection: "column",
+  //     }}
+  //   >
+  //     <TipOfTheDay />
+
+  //     <PokemonList />
+  //     <FormInput onSaveData={onSaveData} setIsSaving={setIsSaving} />
+
+  //     {isSaving && <div>Saving users data...</div>}
+
+  //     <Calculator />
+
+  //     {userData && (
+  //       <MyIntro
+  //         firstName={userData.firstName}
+  //         lastName={userData.lastName}
+  //         age={userData.age}
+  //         male={userData.gender === "Male"}
+  //       />
+  //     )}
+
+  //     <Carousel
+  //       images={[
+  //         "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=30",
+  //         "https://images.unsplash.com/photo-1517052269751-4ae3ad86cc59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=30",
+  //         "https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=30",
+  //       ]}
+  //     />
+  //   </div>
+  // );
+
   return (
-    <div
-      style={{
-        padding: "20px",
-        backgroundColor: "white",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
+    <div>
+      <h1>My app</h1>
 
       <TipOfTheDay />
-
-      <PokemonList />
-      <FormInput onSaveData={onSaveData} setIsSaving={setIsSaving} />
-
-      {isSaving && <div>Saving users data...</div>}
-
-      <Calculator />
-
-      {userData && (
-        <MyIntro
-          firstName={userData.firstName}
-          lastName={userData.lastName}
-          age={userData.age}
-          male={userData.gender === "Male"}
-        />
-      )}
-
-      <Carousel
-        images={[
-          "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=30",
-          "https://images.unsplash.com/photo-1517052269751-4ae3ad86cc59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=30",
-          "https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=30",
-        ]}
-      />
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/pokemon" element={<PokemonList />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/register" element={<FormInput onSaveData={onSaveData} setIsSaving={setIsSaving} />} />
+          <Route
+            path="/carousel"
+            element={
+              <Carousel
+                images={[
+                  "https://images.unsplash.com/photo-1519500099198-fd81846b8f03?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=30",
+                  "https://images.unsplash.com/photo-1517052269751-4ae3ad86cc59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=30",
+                  "https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=30",
+                ]}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
